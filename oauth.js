cc39e76810c46eb01ba5d014a000fe77dee7e919
Peng Yen Chao <mars.90226@gmail.com>
2014-06-05 02:10:48 +0800

    Modify Facebook OAuth settings

--- oauth.js
+++ oauth.js
@@ -2,7 +2,7 @@ var ids = {
   facebook: {
     clientID: '1492379880992323',
     clientSecret: '9f8e1e8a63b77992e11014f02a453d54',
-    callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    callbackURL: process.env.FACEBOOK_OAUTH_CALLBACK || 'http://localhost:3000/auth/facebook/callback'
   }
 };
 
